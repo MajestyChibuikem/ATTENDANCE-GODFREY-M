cea86cd889f03a2987c2f345ddc14de8a1074f14
Majesty <MajestyChibuikem@gmail.com>
2024-11-18 14:24:25 +0100

    finished up the model, started the routes

--- app-api/app.py
+++ app-api/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required
 import sqlite3
@@ -10,4 +11,9 @@ load_dotenv()
 app = Flask(__name__)
 # this key is random for now
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_KEY")
+app.config['SQLALCHEMY_DATABASE_URL'] =os.getenv('DATABASE_URL', 'sqlite:///database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 jwt = JWTManager(app)
+
+db = SQLAlchemy(app)
+# from routes.users
